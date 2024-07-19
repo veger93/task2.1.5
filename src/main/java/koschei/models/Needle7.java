d4cac445cc49fe3bfc2c2acebfd672761d26a07d
Vegerin Sergey <veger93@mail.ru>
2024-07-19 12:44:26 +0300

    Первое сохранение

---
+++ src/main/java/koschei/models/Needle7.java
@@ -0,0 +1,18 @@
+package koschei.models;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Needle7 {
+
+    private Deth8 deth;
+
+    public Needle7(Deth8 deth) {
+        this.deth = deth;
+    }
+
+    @Override
+    public String toString() {
+        return ", смерть Кощея на игле :( ";
+    }
+}
